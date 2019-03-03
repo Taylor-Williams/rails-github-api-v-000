d30e53eb1948535858646f96d1e02122feb959c5
Taylor Williams <taylor@thewilliams.net>
2019-03-03 23:07:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.params
+      req.params['client_id'] = ENV['GITHUB_CLIENT_id']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params['code']
     end
   end
 end
